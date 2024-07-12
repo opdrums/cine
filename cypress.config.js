5bf3f8fc531733e5c47f229b21aeabe1e38ab382
opdrums <operez6921@gmail.com>
2024-07-12 08:54:59 -0500

    add firt test

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
+    specPattern: 'cypress/e2e/**/*.spec.js',
+    video:true,
+    videoCompression:32,
+    reporter: 'mochawesome',
   },
 });
---
+++ cypress/e2e/web/login.spec.js
@@ -0,0 +1,5 @@
+describe('es una prueba a cinemark',()=>{
+    it('', () => {
+        
+    });
+})
